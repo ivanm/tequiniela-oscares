787547402665078694235637eb2a53234ae8ba99
Ivan Mayoral <iimayoral@gmail.com>
2023-02-23 21:25:05 -0600

    Removing unused stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react(), eslintPlugin()],
-  base:
-    process.env.NODE_ENV === "production" ? "/" : undefined,
+  plugins: [react()],
 });
