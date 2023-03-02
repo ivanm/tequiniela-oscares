3131990a569472fa36e54e4d63120ed5a29399bd
Ivan Mayoral <iimayoral@gmail.com>
2023-03-01 18:34:40 -0600

    Using navigate to redirect

--- src/Header.tsx
+++ src/Header.tsx
@@ -178,7 +178,7 @@ export const Header = () => {
                   <MenuItem
                     onClick={async () => {
                       await auth.signOut();
-                      window.location.replace("/");
+                      navigate("/", { replace: true });
                     }}
                   >
                     Logout
