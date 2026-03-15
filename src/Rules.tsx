e393aac1a0b35b57680a6275ae655afd3b640178
Ivan Mayoral <iimayoral@gmail.com>
2026-03-14 21:30:33 -0600

    styling

--- src/Rules.tsx
+++ src/Rules.tsx
@@ -5,7 +5,6 @@ import {
   Text,
   Link,
   Badge,
-  Box,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { AtSignIcon } from "@chakra-ui/icons";
@@ -28,199 +27,93 @@ export const Rules = () => {
         "2xl": "0 250px",
       }}
     >
-      <Flex pl={3} pr={3} pt={3} pb={3} mt={2} align="center">
+      <Flex p={3} mt={2} align="center" gap={2}>
         <Image
           filter={iconFilter}
           src="rules.svg"
           boxSize="25px"
           alt="Rules Figure"
         />
-        <Flex align="center" minHeight="45px">
-          <Heading
-            fontWeight="extrabold"
-            as="h1"
-            fontSize={{
-              base: "19px",
-              lg: "20px",
-            }}
-            mr={2}
-            ml={2}
-          >
-            {t("rules.title")}
-          </Heading>
-        </Flex>
+        <Heading fontWeight="extrabold" as="h1" fontSize={{ base: "19px", lg: "20px" }}>
+          {t("rules.title")}
+        </Heading>
       </Flex>
-      <Flex pl={2} pr={2} direction="column" gap={2}>
+      <Flex px={2} direction="column" gap={2}>
         <Text>{t("rules.welcome")}</Text>
         <Text>{t("rules.joinFun")}</Text>
         <Text>{t("rules.forEveryone")}</Text>
         <Text>{t("rules.getReady")}</Text>
       </Flex>
-      <Flex pl={3} pr={3} pt={3} pb={3} mt={2} align="center">
+
+      <Flex p={3} mt={2} align="center" gap={2}>
         <Image
           filter={iconFilter}
           src="ranking.svg"
           boxSize="25px"
           alt="Ranking Figure"
         />
-        <Flex align="center" minHeight="45px">
-          <Heading
-            fontWeight="extrabold"
-            as="h1"
-            fontSize={{
-              base: "19px",
-              lg: "20px",
-            }}
-            mr={2}
-            ml={2}
-          >
-            {t("rules.prizesTitle")}
-          </Heading>
+        <Heading fontWeight="extrabold" as="h1" fontSize={{ base: "19px", lg: "20px" }}>
+          {t("rules.prizesTitle")}
+        </Heading>
+      </Flex>
+      <Flex px={2} direction="column" gap={3}>
+        <Flex align="baseline" gap={2} flexWrap="wrap">
+          <Badge colorScheme="yellow" fontSize="sm">{t("rules.prizeFirst")}</Badge>
+          <Text fontWeight="bold">{t("rules.prize1")}</Text>
+          <Text fontSize="sm" opacity={0.7}>
+            {t("rules.donatedBy")}{" "}
+            <Link href="https://www.threads.com/@masiosare" isExternal>
+              @masiosare
+            </Link>
+          </Text>
+        </Flex>
+        <Flex align="baseline" gap={2} flexWrap="wrap">
+          <Badge colorScheme="gray" fontSize="sm">{t("rules.prizeSecond")}</Badge>
+          <Text fontWeight="bold">{t("rules.prize2")}</Text>
+          <Text fontSize="sm" opacity={0.7}>
+            {t("rules.donatedBy")}{" "}
+            <Link href="https://twitter.com/thewarpaint" isExternal>
+              @thewarpaint
+            </Link>
+          </Text>
+        </Flex>
+        <Flex align="baseline" gap={2} flexWrap="wrap">
+          <Badge colorScheme="orange" fontSize="sm">{t("rules.prizeThird")}</Badge>
+          <Text fontWeight="bold">{t("rules.prize3")}</Text>
+          <Text fontSize="sm" opacity={0.7}>
+            {t("rules.donatedBy")}{" "}
+            <Link href="https://www.threads.com/@o.ladino" isExternal>
+              @Cani
+            </Link>
+          </Text>
         </Flex>
       </Flex>
-      <Flex pl={2} pr={2} direction="column" gap={4}>
-        <Flex
-          align="center"
-          gap={3}
-          p={3}
-          borderRadius="md"
-          bg="rgba(161, 147, 19, 0.15)"
-          borderLeft="4px solid #a19313"
-        >
-          <Badge
-            colorScheme="yellow"
-            fontSize="md"
-            px={2}
-            py={1}
-            borderRadius="md"
-          >
-            {t("rules.prizeFirst")}
-          </Badge>
-          <Box>
-            <Text fontWeight="bold" fontSize="lg">
-              {t("rules.prize1")}
-            </Text>
-            <Text fontSize="sm" opacity={0.7}>
-              {t("rules.donatedBy")}{" "}
-              <Link href="https://www.threads.com/@masiosare" isExternal>
-                @masiosare
-              </Link>
-            </Text>
-          </Box>
-        </Flex>
-        <Flex
-          align="center"
-          gap={3}
-          p={3}
-          borderRadius="md"
-          bg="rgba(113, 113, 113, 0.15)"
-          borderLeft="4px solid #717171"
-        >
-          <Badge
-            colorScheme="gray"
-            fontSize="md"
-            px={2}
-            py={1}
-            borderRadius="md"
-          >
-            {t("rules.prizeSecond")}
-          </Badge>
-          <Box>
-            <Text fontWeight="bold" fontSize="lg">
-              {t("rules.prize2")}
-            </Text>
-            <Text fontSize="sm" opacity={0.7}>
-              {t("rules.donatedBy")}{" "}
-              <Link href="https://twitter.com/thewarpaint" isExternal>
-                @thewarpaint
-              </Link>
-            </Text>
-          </Box>
-        </Flex>
-        <Flex
-          align="center"
-          gap={3}
-          p={3}
-          borderRadius="md"
-          bg="rgba(114, 81, 18, 0.15)"
-          borderLeft="4px solid #725112"
-        >
-          <Badge
-            colorScheme="orange"
-            fontSize="md"
-            px={2}
-            py={1}
-            borderRadius="md"
-          >
-            {t("rules.prizeThird")}
-          </Badge>
-          <Box>
-            <Text fontWeight="bold" fontSize="lg">
-              {t("rules.prize3")}
-            </Text>
-            <Text fontSize="sm" opacity={0.7}>
-              {t("rules.donatedBy")}{" "}
-              <Link href="https://www.threads.com/@o.ladino" isExternal>
-                @Cani
-              </Link>
-            </Text>
-          </Box>
-        </Flex>
-      </Flex>
-      <Flex pl={3} pr={3} pt={3} pb={3} mt={2} align="center">
+
+      <Flex p={3} mt={2} align="center" gap={2}>
         <AtSignIcon boxSize="25px" />
-        <Flex align="center" minHeight="45px">
-          <Heading
-            fontWeight="extrabold"
-            as="h1"
-            fontSize={{
-              base: "19px",
-              lg: "20px",
-            }}
-            mr={2}
-            ml={2}
-          >
-            {t("rules.teamTitle")}
-          </Heading>
-        </Flex>
+        <Heading fontWeight="extrabold" as="h1" fontSize={{ base: "19px", lg: "20px" }}>
+          {t("rules.teamTitle")}
+        </Heading>
       </Flex>
-      <Flex pl={2} pr={2} direction="column" gap={2}>
+      <Flex px={2} direction="column" gap={2}>
         <Text>
           <b>{t("rules.code")}:</b>{" "}
-          <Link href="https://www.threads.net/@o.ladino" isExternal>
-            @canitoy
-          </Link>
-          ,{" "}
-          <Link href="https://mstdn.social/@Masiosare" isExternal>
-            @Masiosare
-          </Link>
-          ,{" "}
-          <Link href="https://twitter.com/vaan" isExternal>
-            @vaan
-          </Link>
+          <Link href="https://www.threads.net/@o.ladino" isExternal>@canitoy</Link>,{" "}
+          <Link href="https://mstdn.social/@Masiosare" isExternal>@Masiosare</Link>,{" "}
+          <Link href="https://twitter.com/vaan" isExternal>@vaan</Link>
         </Text>
         <Text>
           <b>{t("rules.direction")}</b>: Enrique Segoviano
         </Text>
         <Text>
           <b>{t("rules.design")}</b>:{" "}
-          <Link href="https://twitter.com/chrispulze" isExternal>
-            @chrispulze
-          </Link>
+          <Link href="https://twitter.com/chrispulze" isExternal>@chrispulze</Link>
         </Text>
         <Text>
           <b>{t("rules.prizes")}:</b>{" "}
-          <Link href="https://www.threads.com/@masiosare" isExternal>
-            @masiosare
-          </Link>
-          ,{" "}
-          <Link href="https://twitter.com/thewarpaint" isExternal>
-            @thewarpaint
-          </Link>
-          ,{" "}
-          <Link href="https://www.threads.com/@o.ladino" isExternal>
-            @Cani
-          </Link>
+          <Link href="https://www.threads.com/@masiosare" isExternal>@masiosare</Link>,{" "}
+          <Link href="https://twitter.com/thewarpaint" isExternal>@thewarpaint</Link>,{" "}
+          <Link href="https://www.threads.com/@o.ladino" isExternal>@Cani</Link>
         </Text>
       </Flex>
     </Flex>
